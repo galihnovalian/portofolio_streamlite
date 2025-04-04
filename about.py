6e73930f9841a753290e722d1a821cb6acadbc88
galihnovalian <galihnovalian.gn@gmail.com>
2025-04-04 17:39:56 +0800

    Update about.py

--- about.py
+++ about.py
@@ -4,4 +4,4 @@ def about_me():
     st.subheader('Nama saya adalah Galih Novalian')
     st.write('Saya seorang Data Scientist Enthusiast')
     st.write('Saat ini saya sedang mengambil kelas bootcamp di Dibimbing')
-    st.write('Sata ini saya bekerja di perusahaan Tambang Petrosea')
+    st.write('Saat ini saya bekerja di perusahaan Tambang Petrosea')
