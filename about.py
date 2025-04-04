212f0fd05e9800b13aea5f39a5e70c39f582a0d5
galihnovalian <galihnovalian.gn@gmail.com>
2025-04-04 17:15:01 +0800

    Pembuatan Aplikasi

---
+++ about.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def about_me():
+    st.subheader('Nama saya adalah Galih')
+    st.write('Saya seorang Data Scientist Enthusiast')
+    st.write('Sata ini saya bekejra di perusahaan Tambang')
