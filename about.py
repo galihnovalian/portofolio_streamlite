75604943afc6c3b5e55dfa34e33361d5fcd14c00
galihnovalian <galihnovalian.gn@gmail.com>
2025-04-04 17:37:53 +0800

    Update about.py

--- about.py
+++ about.py
@@ -3,4 +3,5 @@ import streamlit as st
 def about_me():
     st.subheader('Nama saya adalah Galih Novalian')
     st.write('Saya seorang Data Scientist Enthusiast')
-    st.write('Sata ini saya bekerja di perusahaan Tambang')
+    st.write('Saat ini saya sedang mengambil kelas bootcamp di Dibimbing')
+    st.write('Sata ini saya bekerja di perusahaan Tambang Petrosea')
